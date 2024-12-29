93ab42bd76a4074617e3d95cc4ad9768b5756f3c
TK3096 <tongthanaphon.tk@gmail.com>
2024-12-29 16:30:13 +0700

    init

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactStrictMode: true,
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+import nextra from "nextra";
+
+const withNextra = nextra({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
+
+export default withNextra({});
